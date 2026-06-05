60c92949bebee2d5fe9ba99781545d2d7e059b0b
Vrashabh Sontakke <vrashabh@sontakke.in>
2026-06-05 15:20:52 +0530

    update nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Examera dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            flutter
+            postgresql_15
+            gnumake
+          ];
+
+          shellHook = ''
+            export GOPATH="$HOME/go"
+            export PATH="$GOPATH/bin:$PATH"
+            export DATABASE_URL="postgres://examera:examera_pass@localhost:5432/examera_db?sslmode=disable"
+          '';
+        };
+      }
+    );
+}
